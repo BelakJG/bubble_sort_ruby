89f52da8b23c8e6e6dbed2b27d0fb1a0df97b9b9
BelakJG <74761299+BelakJG@users.noreply.github.com>
2025-12-29 13:30:02 -0500

    added rubocop

--- bubble.rb
+++ bubble.rb
@@ -1,19 +1,17 @@
 def bubble_sort(unsorted_arr)
   swapped = true
-  until swapped==false
+  until swapped == false
     swapped = false
     unsorted_arr[0..-2].each_index do |index|
-      if (unsorted_arr[index] > unsorted_arr[index+1])
-        temp = unsorted_arr[index]
-        unsorted_arr[index] = unsorted_arr[index+1]
-        unsorted_arr[index+1] = temp
-        swapped = true
-      end
+      next unless unsorted_arr[index] > unsorted_arr[index + 1]
+
+      temp = unsorted_arr[index]
+      unsorted_arr[index] = unsorted_arr[index + 1]
+      unsorted_arr[index + 1] = temp
+      swapped = true
     end
   end
   unsorted_arr
 end
 
-
-
-p bubble_sort([4,3,78,2,0,2,-1,5,10,99])
+p bubble_sort([4, 3, 78, 2, 0, 2, -1, 5, 10, 99])
